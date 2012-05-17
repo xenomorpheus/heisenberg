f6697c3690304f0bdeb2c03982411d2ab6a7ff79
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-05-17 12:56:29 +0000

    Added a test suite.
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4464 30b58a95-ce55-df11-8e28-0015176d015e

---
+++ src/AllTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ CoinCollectionTest.class, ItemTest.class, RingTest.class })
+public class AllTests {
+
+}
