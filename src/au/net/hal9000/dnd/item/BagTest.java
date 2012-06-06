2131b9816d0b2d9341450a5b5385bdb979d8cc24
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-06-06 02:37:41 +0000

    Added comments.
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4515 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/dnd/item/BagTest.java
+++ src/au/net/hal9000/dnd/item/BagTest.java
@@ -11,6 +11,8 @@ import au.net.hal9000.dnd.item.exception.ExceptionTooHeavy;
 public class BagTest {
 
 	
+	// Normal item into bag.
+	// item's location changes to bag
 	@Test
 	public void ordinary_add() {
 		Cookie cookie = new Cookie();
@@ -28,7 +30,8 @@ public class BagTest {
 		assertEquals("cookie location",bag,cookie.getLocation());
 	}
 	
-	
+	// Sharp items throw ExceptionInvalidType
+	// Item's location remains unchanged.
 	@Test
 	public void sharp_add_rupture() {
 		Sword sword = new Sword();
