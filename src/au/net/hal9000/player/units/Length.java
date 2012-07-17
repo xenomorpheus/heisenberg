e471227661feff949db3cf471b5a63e81cf319df
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-17 21:32:30 +0000

    Adding explicit calls to super() and constuctors. Adding comments for code review.
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4658 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/player/units/Length.java
+++ src/au/net/hal9000/player/units/Length.java
@@ -1,10 +1,34 @@
 package au.net.hal9000.player.units;
 
-public class Length extends Unit {
+import java.io.Serializable;
+
+/** A representation of length.
+ * 
+ * @see		au.net.hal9000.player.units.Unit
+ * @author bruins
+ *
+ */
+public class Length extends Unit implements Serializable, Cloneable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+	// TODO is this explicit constructor best practice?
+	public Length(){
+		// TODO is this explicit super() J.B.P.?
+		super();
+	}
+	// TODO is this explicit constructor best practice?
+	public Length(float f){
+		// TODO is this explicit super() J.B.P.?
+		super(f);
+	}
+
+	public Length clone() throws CloneNotSupportedException {
+		// Make sure the cloning is deep, not shallow.
+		// e.g. set the non-mutable, non-primitives
+		return (Length) super.clone();
+	}
 }
