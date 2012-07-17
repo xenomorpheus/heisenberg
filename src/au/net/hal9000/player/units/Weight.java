804962fd267e7671f5938ce14e7b65d622ed2323
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-17 21:29:29 +0000

    Explicitly marking as Serializable. Adding JavaDoc
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4656 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/player/units/Weight.java
+++ src/au/net/hal9000/player/units/Weight.java
@@ -1,18 +1,36 @@
 package au.net.hal9000.player.units;
 
-public class Weight extends Unit implements Cloneable {
+import java.io.Serializable;
+
+/** A representation of weight.
+ * 
+ * @author bruins
+ *
+ */
+public class Weight extends Unit implements Serializable, Cloneable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public Weight(){
+
+	// Constructors
+	public Weight() {
 		super();
 	}
-	public Weight(float f){
+
+	public Weight(float f) {
 		super(f);
 	}
 
+    // Methods
+	// TODO is this required?
+	/**
+	 * Returns a clone of the current object.
+	 * 
+	 * @return Weight
+	 * 
+	 */
 	public Weight clone() throws CloneNotSupportedException {
 		// Make sure the cloning is deep, not shallow.
 		// e.g. set the non-mutable, non-primitives
