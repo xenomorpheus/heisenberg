84bc372c491242089846ef60a96760994b76a8ed
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-17 21:33:40 +0000

    Adding JavaDoc
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4659 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/player/units/Currency.java
+++ src/au/net/hal9000/player/units/Currency.java
@@ -6,14 +6,37 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 // This is NOT an Item
+/**
+ * A representation of currency, value, wealth.
+ * 
+ * Currency contains a whole number of pieces (coins) of each of the following:
+ * copper, silver, gold and platinum. These are abbreviated as CP, SP, GP and PP
+ * respectively.
+ * 
+ * Objects are mutable.
+ * 
+ * @author bruins
+ * 
+ */
 public class Currency implements Serializable, Cloneable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	// conversion
+
+	/**
+	 * Coin conversion: 1PP = 10 GP.
+	 */
 	public static final Float ppAsGp = 10F;
+
+	/**
+	 * Coin conversion: 1GP = 10 SP.
+	 */
 	public static final Float spAsGp = 0.1F;
+
+	/**
+	 * Coin conversion: 1 SP = 10 GP.
+	 */
 	public static final Float cpAsGp = 0.01F;
 
 	// coin count in this collection
@@ -22,23 +45,51 @@ public class Currency implements Serializable, Cloneable {
 	private int sp = 0;
 	private int cp = 0;
 
+	// Constructors
+	/**
+	 * Creates a zero value Currency object that can be modified later.
+	 * 
+	 */
 	public Currency() {
+		super();
 	}
 
+	/**
+	 * Creates a Currency object that can be modified later.
+	 * 
+	 * @param pPp
+	 *            Number of Platinum Pieces in this object.
+	 * @param pGp
+	 *            Number of Gold Pieces in this object.
+	 * @param pSp
+	 *            Number of Silver Pieces in this object.
+	 * @param pCp
+	 *            Number of Copper Pieces in this object.
+	 */
 	public Currency(int pPp, int pGp, int pSp, int pCp) {
+		super();
 		this.pp = pPp;
 		this.gp = pGp;
 		this.sp = pSp;
 		this.cp = pCp;
 	}
 
-	public Currency(Currency cc) {
-		this.pp = cc.pp;
-		this.gp = cc.gp;
-		this.sp = cc.sp;
-		this.cp = cc.cp;
+	/**
+	 * Clones an exiting Currency object. Each can be independently modified
+	 * later.
+	 * 
+	 * @param otherCollection
+	 *            The Currency object being cloned.
+	 */
+	public Currency(Currency otherCollection) {
+		super();
+		this.pp = otherCollection.pp;
+		this.gp = otherCollection.gp;
+		this.sp = otherCollection.sp;
+		this.cp = otherCollection.cp;
 	}
-    // static methods
+
+	// static methods
 	public static boolean null_safe_equals(Currency currency,
 			Currency currencyOther) {
 		if (currency == null) {
@@ -60,64 +111,101 @@ public class Currency implements Serializable, Cloneable {
 		return (pp * ppAsGp) + gp + (sp * spAsGp) + (cp * cpAsGp);
 	}
 
+	/**
+	 * Get the number of platinum pieces.
+	 */
 	public int getPp() {
 		return pp;
 	}
 
+	/**
+	 * Set the number of platinum pieces.
+	 * 
+	 * @param pp
+	 *            The number of platinum pieces.
+	 */
 	public void setPp(int pp) {
 		this.pp = pp;
 	}
 
+	/**
+	 * Get the number of gold pieces.
+	 */
 	public int getGp() {
 		return gp;
 	}
 
+	/**
+	 * Set the number of gold pieces.
+	 * 
+	 * @param gp
+	 *            The number of gold pieces.
+	 */
 	public void setGp(int gp) {
 		this.gp = gp;
 	}
 
+	/**
+	 * Get the number of silver pieces.
+	 */
 	public int getSp() {
 		return sp;
 	}
 
+	/**
+	 * Set the number of silver pieces.
+	 * 
+	 * @param sp
+	 *            The number of silver pieces.
+	 */
 	public void setSp(int sp) {
 		this.sp = sp;
 	}
 
+	/**
+	 * Get the number of copper pieces.
+	 */
 	public int getCp() {
 		return cp;
 	}
 
+	/**
+	 * Set the number of coper pieces.
+	 * 
+	 * @param cp
+	 *            The number of coper pieces.
+	 */
 	public void setCp(int cp) {
 		this.cp = cp;
 	}
 
 	// increment the current object by the argument currency.
 	// note that the currency arg is not changed.
-	public void add(Currency cc) {
+	public void add(Currency otherCollection) {
 		// add the value to the current Currency.
-		this.pp += cc.pp;
-		this.gp += cc.gp;
-		this.sp += cc.sp;
-		this.cp += cc.cp;
+		this.pp += otherCollection.pp;
+		this.gp += otherCollection.gp;
+		this.sp += otherCollection.sp;
+		this.cp += otherCollection.cp;
 	}
 
 	// transfer all the value of the passed currency object.
-	public void transfer(Currency cc) {
-		this.add(cc);
-		// remove the value from the original cc.
-		cc.pp = cc.gp = cc.sp = cc.cp = 0;
+	public void transfer(Currency otherCollection) {
+		this.add(otherCollection);
+		// remove the value from the original otherCollection.
+		otherCollection.pp = otherCollection.gp = otherCollection.sp = otherCollection.cp = 0;
 	}
 
-	public boolean equals(Currency cc) {
-		return (pp == cc.pp) && (gp == cc.gp) && (sp == cc.sp) && (cp == cc.cp);
+	public boolean equals(Currency otherCollection) {
+		return (pp == otherCollection.pp) && (gp == otherCollection.gp)
+				&& (sp == otherCollection.sp) && (cp == otherCollection.cp);
 	}
 
 	public String toString() {
 		return new String(this.pp + "pp, " + this.gp + "gp, " + this.sp
 				+ "sp, " + this.cp + "cp ");
 	}
-	
+
 	private void writeObject(ObjectOutputStream out) throws IOException {
 		out.defaultWriteObject();
 	}
@@ -137,5 +225,5 @@ public class Currency implements Serializable, Cloneable {
 		// e.g. set the non-mutable, non-primitives
 
 		return clone;
-	}	
+	}
 }
