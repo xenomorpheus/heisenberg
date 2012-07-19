eddccbf7836533752b61b9ba20aec3afc881ae37
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-19 14:00:12 +0000

    Added dice and unit tests.
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4676 30b58a95-ce55-df11-8e28-0015176d015e

---
+++ src/au/net/hal9000/player/util/AllTests.java
@@ -0,0 +1,11 @@
+package au.net.hal9000.player.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DiceTest.class })
+public class AllTests {
+
+}
