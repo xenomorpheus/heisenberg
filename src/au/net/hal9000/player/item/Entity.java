57dc198d9a8f92092f0f98096ac64b3e29611136
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-22 14:37:00 +0000

    Entities now have worn items.
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4708 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/player/item/Entity.java
+++ src/au/net/hal9000/player/item/Entity.java
@@ -18,11 +18,15 @@ public abstract class Entity extends ItemImpl {
 	 */
 	private static final long serialVersionUID = 1L;
 	/**
-	 * A list of items ({@link Item}) carried.
+	 * A list of items ({@link Item}) stowed.
 	 */
-	private ItemContainer equipment = new Bag("Equipment");
+	private ItemContainer worn = new Box("Worn");
 	/**
 	 * A list of {@link Ingredient} list that is known by this Entity.
+	 */
+	private ItemContainer equipment = new Box("Equipment");
+	/**
+	 * A list of items ({@link Item}) worn.
 	 */
 	private ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
 	/**
@@ -52,6 +56,14 @@ public abstract class Entity extends ItemImpl {
 		return equipment;
 	}
 
+	public void setWorn(ItemContainer worn) {
+		this.worn = worn;
+	}
+
+	public ItemContainer getWorn() {
+		return worn;
+	}
+
 	// ingredients
 	public ArrayList<Ingredient> getIngredients() {
 		return ingredients;
@@ -72,6 +84,17 @@ public abstract class Entity extends ItemImpl {
 
 	// Misc
 	/**
+	 * Wear an item
+	 * 
+	 * @param item
+	 */
+	public void wear(ItemImpl item) {
+		// TODO check the item is wearable.
+		worn.add(item);
+	}
+	// TODO add unwear
+
+	/**
 	 * Add item to equipment
 	 * 
 	 * @param item
@@ -90,6 +113,9 @@ public abstract class Entity extends ItemImpl {
 	@Override
 	public Weight getWeight() {
 		Weight total = super.getWeight();
+		if (total == null)
+			total = new Weight();
+		total.add(worn.getWeight());
 		total.add(equipment.getWeight());
 		return total;
 	}
@@ -104,6 +130,9 @@ public abstract class Entity extends ItemImpl {
 	@Override
 	public Volume getVolume() {
 		Volume total = super.getVolume();
+		if (total == null)
+			total = new Volume();
+		total.add(worn.getVolume());
 		total.add(equipment.getVolume());
 		return total;
 	}
@@ -118,6 +147,7 @@ public abstract class Entity extends ItemImpl {
 	@Override
 	public Currency getValue() {
 		Currency total = super.getValue();
+		total.add(worn.getValue());
 		total.add(equipment.getValue());
 		return total;
 	}
@@ -138,14 +168,19 @@ public abstract class Entity extends ItemImpl {
 
 	// Traverse Tree
 	/** {@inheritDoc} */
+	@Override
 	public boolean isLeaf() {
 		return false;
 	}
 
 	/** {@inheritDoc} */
+	@Override
 	public int getChildCount() {
 		// TODO consider Items in super
 		int count = super.getChildCount();
+		if (worn != null) {
+			count++;
+		}
 		if (equipment != null) {
 			count++;
 		}
@@ -153,12 +188,31 @@ public abstract class Entity extends ItemImpl {
 	}
 
 	/** {@inheritDoc} */
+	@Override
 	public ItemImpl getChild(int index) {
-		// TODO consider Items in super
-		int count = super.getChildCount();
-		// Note the zero offset
-		if (index == count) {
-			return equipment;
+		// Note: index is zero offset, count is not.
+		int child_count = super.getChildCount();		
+		// Child is on super.
+        if (index < child_count){
+        	return (ItemImpl)super.getChild(index);
+        }
+        // Make index relative to this class;
+        index -= child_count;
+
+        // worn
+		if (worn != null){
+			if (index == 0) {
+				return worn;
+			}
+			index--;
+		}
+
+		// equipment
+		if (equipment != null){
+			if (index == 0) {
+				return equipment;
+			}
+			index--;
 		}
 		return null;
 	}
