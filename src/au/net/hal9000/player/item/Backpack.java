aee11f2d391853849c927ebc313f34246523cecf
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-22 14:40:50 +0000

    simplified constructor
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4715 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/player/item/Backpack.java
+++ src/au/net/hal9000/player/item/Backpack.java
@@ -7,7 +7,7 @@ public class Backpack extends Bag {
 	private static final long serialVersionUID = 1L;
 
 	public Backpack() {
-		super("Backpack");
+		this("Backpack");
 	}
 
 	public Backpack(String pString) {
