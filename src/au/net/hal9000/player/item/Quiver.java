65eb0c116bebd4cea3fc35598e196062303b3384
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-22 14:45:00 +0000

    simplified
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4719 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/player/item/Quiver.java
+++ src/au/net/hal9000/player/item/Quiver.java
@@ -1,28 +1,22 @@
 package au.net.hal9000.player.item;
 
-import au.net.hal9000.player.item.exception.ExceptionInvalidType;
-import au.net.hal9000.player.item.exception.ExceptionTooBig;
-import au.net.hal9000.player.item.exception.ExceptionTooHeavy;
-
 public class Quiver extends ItemContainer {
-	
-  /**
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-public Quiver(){
-	  super("Quiver");
-  }
-  public Quiver(String pName){
-	  super(pName);
-  }
+	public Quiver() {
+		this("Quiver");
+	}
 
-  public void add (ItemImpl pItem) throws ExceptionInvalidType, ExceptionTooHeavy, ExceptionTooBig{
-	  if (! (pItem instanceof Arrow)){
-		  throw new ExceptionInvalidType(pItem.getName()+" is not a Arrow");
-	  }
-	  super.add(pItem);
-  }
-  
+	public Quiver(String pName) {
+		super(pName);
+	}
+
+	public void add(Arrow pArrow) {
+		super.add(pArrow);
+	}
+
 }
