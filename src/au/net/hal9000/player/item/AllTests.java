2d8a80b8bcdc4238645b0d94cd8244b79282a60f
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-23 01:03:25 +0000

    added new test classes
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4729 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/player/item/AllTests.java
+++ src/au/net/hal9000/player/item/AllTests.java
@@ -5,10 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BagOfHoldingTest.class, BagTest.class, CookieTest.class,
-		CrossbowTest.class, EntityTest.class, HandTest.class,
-		HumanoidTest.class, ItemContainerTest.class, ItemImplTest.class,
-		LocationTest.class, MagicRingTest.class, RingTest.class })
+@SuiteClasses({ BagOfHoldingTest.class, BagTest.class, BoxTest.class,
+		CloakTest.class, CookieTest.class, CrossbowTest.class,
+		EntityTest.class, HandTest.class, HorseTest.class, HumanoidTest.class,
+		ItemContainerTest.class, ItemImplTest.class, LocationTest.class,
+		MagicRingTest.class, QuiverTest.class, RingTest.class,
+		ScabbardTest.class, ShieldTest.class })
 public class AllTests {
 
 }
