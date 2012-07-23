815b2b3642bb575086f9958c09d00169115f14e7
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-23 15:02:32 +0000

    isSharp, java doc
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4748 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/player/item/Arrow.java
+++ src/au/net/hal9000/player/item/Arrow.java
@@ -6,11 +6,19 @@ public class Arrow extends Item {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	// Constructor
 	public Arrow() {
 		this("Arrow");
 	}
 
 	public Arrow(String pString) {
 		super(pString);
+	}
+
+	// Feature
+	/** {@inheritDoc} */
+	@Override
+	public boolean isSharp() {
+		return true;
 	}
 }
