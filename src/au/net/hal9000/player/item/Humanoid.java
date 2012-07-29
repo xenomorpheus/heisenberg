aba138f5dff2b156969eb026d9159485487cc2ec
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-07-29 09:37:27 +0000

    Added a head to a humanoid.  Added tests for equals
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4783 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/player/item/Humanoid.java
+++ src/au/net/hal9000/player/item/Humanoid.java
@@ -18,19 +18,36 @@ public abstract class Humanoid extends Entity {
 	}
 
 	// Getters and Setters
+	// Head
 	public HumanoidHead getHead() {
 		return head;
 	}
 
+	public void setHead(HumanoidHead head) {
+		this.head = head;
+	}
+
+	// left Hand
 	public Hand getLeftHand() {
 		return leftHand;
 	}
 
+	public void setLeftHand(Hand hand) {
+		this.leftHand = hand;
+	}
+
+	// right Hand
 	public Hand getRightHand() {
 		return rightHand;
 	}
 
+	public void setRightHand(Hand hand) {
+		this.rightHand = hand;
+	}
+
 	// Misc
+	/** {@inheritDoc} */
+	@Override
 	public Weight getWeight() {
 		Weight total = super.getWeight();
 		total.add(head.getWeight());
@@ -43,6 +60,8 @@ public abstract class Humanoid extends Entity {
 		return total;
 	}
 
+	/** {@inheritDoc} */
+	@Override
 	public Volume getVolume() {
 		Volume total = super.getVolume();
 		total.add(head.getVolume());
@@ -55,13 +74,22 @@ public abstract class Humanoid extends Entity {
 		return total;
 	}
 
-	
-	public boolean equals(Humanoid pHumanoid) {
-		// TODO
-		throw new RuntimeException("Can't do humanoid equals yet"
-				+ this.getName());
+	public boolean equals(Humanoid other) {
+		if (!head.equals(other.getHead()))
+			return false;
+		if (leftHand != null) {
+			if (!leftHand.equals(other.getLeftHand()))
+				return false;
+		}
+		if (rightHand != null) {
+			if (!rightHand.equals(other.getRightHand()))
+				return false;
+		}
+		return super.equals(other);
 	}
 
+	/** {@inheritDoc} */
+	@Override
 	public void beNot() {
 		// Call beNot on the Items directly declared in this class.
 		head.beNot();
--- src/au/net/hal9000/player/item/HumanoidTest.java
+++ src/au/net/hal9000/player/item/HumanoidTest.java
@@ -3,6 +3,8 @@ package au.net.hal9000.player.item;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import au.net.hal9000.player.units.Weight;
+
 public class HumanoidTest {
 
 	@Test
@@ -12,6 +14,64 @@ public class HumanoidTest {
 		human.getLeftHand().setWeightBase(2F);
 		human.getRightHand().setWeightBase(4F);
 		assertTrue("getWeight", human.getWeight().equals(7F));
+	}
+
+	@Test
+	public void testEquals() {
+		Human human = new Human(); // Close enough for this abstract class
+		Human other = new Human();
+		assertTrue("equals basic", human.equals(other));
+
+		// Alter head
+		{
+			HumanoidHead headOld = human.getHead(); // Save old
+			Weight weightMaxNew = headOld.getWeightMax();
+			HumanoidHead headNew = new HumanoidHead();
+			if (weightMaxNew == null){
+				weightMaxNew = new Weight();
+				headNew.setWeightMax(weightMaxNew);
+			}
+			weightMaxNew.add(0.1f);
+			human.setHead(headNew);
+			assertFalse("not equals. head differs", human.equals(other));
+			human.setHead(headOld); // Restore old
+			assertTrue("equals basic", human.equals(other)); // Check restore
+																// old
+		}
+		// Alter leftHand
+		{
+			Hand leftHandOld = human.getLeftHand(); // Save old
+			Weight weightMaxNew = leftHandOld.getWeightMax();
+			Hand leftHandNew = new Hand();
+			if (weightMaxNew == null){
+				weightMaxNew = new Weight();
+				leftHandNew.setWeightMax(weightMaxNew);
+			}
+			weightMaxNew.add(0.1f);
+			human.setLeftHand(leftHandNew);
+			assertFalse("not equals. left hand differ", human.equals(other));
+			human.setLeftHand(leftHandOld); // Restore old
+			assertTrue("equals basic", human.equals(other)); // Check restore
+																// old
+		}
+
+		// Alter rightHand
+		{
+			Hand rightHandOld = human.getRightHand(); // Save old
+			Weight weightMaxNew = rightHandOld.getWeightMax();
+			Hand rightHandNew = new Hand();
+			if (weightMaxNew == null){
+				weightMaxNew = new Weight();
+				rightHandNew.setWeightMax(weightMaxNew);
+			}
+			weightMaxNew.add(0.1f);
+			human.setRightHand(rightHandNew);
+			assertFalse("not equals. right hand differ", human.equals(other));
+			human.setRightHand(rightHandOld); // Restore old
+			assertTrue("equals basic", human.equals(other)); // Check restore
+																// old
+		}
+
 	}
 
 }
