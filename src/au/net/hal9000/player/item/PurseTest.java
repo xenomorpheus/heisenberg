3b3ec737b654d7894c5aa973f5000f749a64e1fe
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2012-08-12 22:25:32 +0000

    started
    git-svn-id: file:///home/svn/prog/java/Player/trunk@4881 30b58a95-ce55-df11-8e28-0015176d015e

---
+++ src/au/net/hal9000/player/item/PurseTest.java
@@ -0,0 +1,39 @@
+package au.net.hal9000.player.item;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PurseTest {
+
+	// TODO put in config
+	private static final float coinsToWeight = 0.1f;
+	// TODO give value and put in config
+	private static final float coinsToVolume = 0.01f;	
+	
+	@Test
+	public void testGetVolume() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPurse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPurseString() {
+		fail("Not yet implemented");
+	}
+
+}
