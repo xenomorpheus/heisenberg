85690966466c83819592e86340bc1ae302778ebd
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2013-02-26 19:43:18 +0000

    Feedback.  Package names should be lowecase.
    git-svn-id: file:///home/svn/prog/java/Heisenberg/trunk@5166 30b58a95-ce55-df11-8e28-0015176d015e

--- src/au/net/hal9000/heisenberg/itemCreator/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-
-GUI Tool. World editor. Add {@link au.net.hal9000.heisenberg.item.Item} objects.
-
-@author Mike Bruins
-@version 1.0
-*/
-package au.net.hal9000.heisenberg.itemCreator;
--- src/au/net/hal9000/heisenberg/pcCreator/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
-
-GUI Tool. Character Sheet Editor. Add {@link au.net.hal9000.heisenberg.item.Item} objects.
-
-
-@author Mike Bruins
-@version 1.0
-*/
-package au.net.hal9000.heisenberg.pcCreator;
