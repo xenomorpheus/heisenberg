d16b49e8c42d51f8b63a95961c3fb6d8b9ac8a21
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2013-03-02 06:11:53 +0000

    JPA working as far as I know.
    git-svn-id: file:///home/svn/prog/java/Heisenberg/trunk@5181 30b58a95-ce55-df11-8e28-0015176d015e

--- src/test/java/au/net/hal9000/heisenberg/item/PersistenceTest.java
+++ src/test/java/au/net/hal9000/heisenberg/item/PersistenceTest.java
@@ -19,7 +19,7 @@ import au.net.hal9000.heisenberg.util.Configuration;
 public class PersistenceTest {
 
     @Test
-    public void testJpaPersistence() {
+    public void oneOfEachItem() {
         Configuration config = null;
         try {
             config = new Configuration("test/config/config.xml");
@@ -40,16 +40,15 @@ public class PersistenceTest {
             // Create a new Item
             em.getTransaction().begin();
             Item item = Factory.createItem(itemClass);
-//            System.out.println("The " + itemClass + " jpaID before="
-//                    + item.getJpaId());
+            assertEquals(0L, item.getJpaId());
             item.setName("This is a " + itemClass + " Name");
             item.setDescription("This is a " + itemClass + " Description");
-            
+
             // Persist it
             em.persist(item);
             em.getTransaction().commit();
             long jpaId = item.getJpaId();
-//            System.out.println("The " + itemClass + " jpaID after=" + jpaId);
+            assertNotEquals(0L, jpaId);
 
             Query q = em.createQuery("select c from " + itemClass
                     + " c where c.jpaId = " + jpaId);
@@ -64,4 +63,119 @@ public class PersistenceTest {
         }
         em.close();
     }
+
+    @Test
+    public void testContainer() {
+        Bag bag = new Bag();
+        Cookie cookie1 = new Cookie();
+        Cookie cookie2 = new Cookie();
+        bag.add(cookie1);
+        bag.add(cookie2);
+        assertEquals(0L, cookie1.getJpaId());
+        assertEquals(0L, cookie2.getJpaId());
+        assertEquals(0L, bag.getJpaId());
+
+        final String PERSISTENCE_UNIT_NAME = "items";
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+
+        // Persist it
+        em.getTransaction().begin();
+        em.persist(bag);
+        em.getTransaction().commit();
+        assertNotEquals(0L, cookie1.getJpaId());
+        assertNotEquals(0L, cookie2.getJpaId());
+        assertNotEquals(0L, bag.getJpaId());
+
+        em.close();
+    }
+
+    public static Location testGetWorld() {
+        // Ad-hoc test world
+        Location world = new Location("World");
+        world.setWeightMax(100000);
+        world.setVolumeMax(100000);
+
+        // Scabbard
+        Scabbard scabbard = new Scabbard();
+        scabbard.add(new Sword());
+
+        Scabbard scabbard2 = new Scabbard("Scabbard2");
+        scabbard2.add(new Sword());
+
+        Bag boh = new BagOfHolding(1);
+        boh.add(new Bag("Bag1"));
+        boh.add(new Box("Box1"));
+        boh.add(new Candle());
+        boh.add(new Cloak());
+        boh.add(new Cookie("Cookie1"));
+        boh.add(new Crossbow());
+        boh.add(new CrossbowBolt());
+        boh.add(new MagicRing());
+        boh.add(new Ring());
+        boh.add(scabbard);
+        boh.add(new Torch());
+
+        // a backpack of stuff
+        Bag backpack = new Backpack("Backpack1");
+        backpack.setWeightMax(100000);
+        backpack.setVolumeMax(100000);
+        backpack.add(boh);
+
+        Quiver quiver = new Quiver();
+        quiver.add(new Arrow());
+        quiver.add(new Arrow());
+        quiver.add(new Arrow());
+
+        Bag bag2 = new Bag("Bag2");
+        bag2.add(new Cookie("Cookie6"));
+        bag2.add(new Cookie("Cookie7"));
+        bag2.add(new Cookie("Cookie8"));
+        backpack.add(bag2);
+
+        // a human with a bag of cookies
+        Human human = new Human("Human1");
+        world.add(human);
+        human.setWeightMax(100000);
+        human.setVolumeMax(100000);
+
+        human.add(new Shield());
+        human.add(scabbard2);
+        human.add(quiver);
+        human.add(backpack);
+
+        world.add(new Sword());
+        world.add(new Horse());
+
+        // bag3
+        Bag bag3 = new Bag("Bag3");
+        bag3.add(new Cookie("Cookie9"));
+        bag3.add(new Cookie("Cookie10"));
+        bag3.add(new Cookie("Cookie11"));
+        world.add(bag3);
+
+        return world;
+    }
+
+    @Test
+    public void testLocation() {
+
+        Location loc = testGetWorld();
+
+        final String PERSISTENCE_UNIT_NAME = "items";
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+
+        // Persist it
+        em.getTransaction().begin();
+        em.persist(loc);
+        em.getTransaction().commit();
+        long jpaId = loc.getJpaId();
+        System.out.println("The loc jpaId=" + jpaId);
+
+        em.close();
+    }
+
 }
