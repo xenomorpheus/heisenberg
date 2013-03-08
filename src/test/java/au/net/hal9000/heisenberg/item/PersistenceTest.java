b95d46219bcc085ea498d7baf329136b806d1586
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2013-03-08 01:20:54 +0000

    No code check warnings.
    git-svn-id: file:///home/svn/prog/java/Heisenberg/trunk@5198 30b58a95-ce55-df11-8e28-0015176d015e

--- src/test/java/au/net/hal9000/heisenberg/item/PersistenceTest.java
+++ src/test/java/au/net/hal9000/heisenberg/item/PersistenceTest.java
@@ -18,162 +18,172 @@ import au.net.hal9000.heisenberg.util.Configuration;
 
 public class PersistenceTest {
 
-    @Test
-    public void oneOfEachItem() {
-        Configuration config = null;
-        try {
-            config = new Configuration("test/config/config.xml");
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            fail(e.getStackTrace().toString());
-        }
-        Vector<String> itemClasses = config.getItemClasses();
+	@Test
+	public void oneOfEachItem() {
+		Configuration config = null;
+		try {
+			config = new Configuration("test/config/config.xml");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			fail(e.getStackTrace().toString());
+		}
+		Vector<String> itemClasses = config.getItemClasses();
 
-        final String PERSISTENCE_UNIT_NAME = "items";
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-        EntityManager em = factory.createEntityManager();
+		final String PERSISTENCE_UNIT_NAME = "items";
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		EntityManager em = factory.createEntityManager();
 
-        for (String itemClass : itemClasses) {
+		for (String itemClass : itemClasses) {
 
-            // Create a new Item
-            em.getTransaction().begin();
-            Item item = Factory.createItem(itemClass);
-            assertEquals(0L, item.getJpaId());
-            item.setName("This is a " + itemClass + " Name");
-            item.setDescription("This is a " + itemClass + " Description");
+			// Create a new Item
+			em.getTransaction().begin();
+			Item item = Factory.createItem(itemClass);
+			assertEquals(0L, item.getJpaId());
+			item.setName("This is a " + itemClass + " Name");
+			item.setDescription("This is a " + itemClass + " Description");
 
-            // Persist it
-            em.persist(item);
-            em.getTransaction().commit();
-            long jpaId = item.getJpaId();
-            assertNotEquals(0L, jpaId);
+			// Persist it
+			em.persist(item);
+			em.getTransaction().commit();
+			long jpaId = item.getJpaId();
+			assertNotEquals(0L, jpaId);
 
-            Query q = em.createQuery("select c from " + itemClass
-                    + " c where c.jpaId = " + jpaId);
-            List<Item> itemList = q.getResultList();
-            assertEquals("select count", 1, itemList.size());
-            for (Item item2 : itemList) {
-                assertEquals("name", "This is a " + itemClass + " Name",
-                        item2.getName());
-                assertEquals("description", "This is a " + itemClass
-                        + " Description", item2.getDescription());
-            }
-        }
-        em.close();
-    }
+			Query q = em.createQuery("select c from " + itemClass
+					+ " c where c.jpaId = " + jpaId);
 
-    @Test
-    public void testContainer() {
-        Bag bag = new Bag();
-        Cookie cookie1 = new Cookie();
-        Cookie cookie2 = new Cookie();
-        bag.add(cookie1);
-        bag.add(cookie2);
-        assertEquals(0L, cookie1.getJpaId());
-        assertEquals(0L, cookie2.getJpaId());
-        assertEquals(0L, bag.getJpaId());
+			Object resultList = q.getResultList();
 
-        final String PERSISTENCE_UNIT_NAME = "items";
-        EntityManagerFactory factory = Persistence
-                .createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-        EntityManager em = factory.createEntityManager();
+			if (!(resultList instanceof List<?>)) {
+				fail("result expected to be a list");
+			}
+			// http://stackoverflow.com/questions/262367/type-safety-unchecked-cast
+			@SuppressWarnings("unchecked")
+			List<Item> itemList = (List<Item>) resultList;
+			assertEquals("select count", 1, itemList.size());
+			for (Item item2 : itemList) {
+				assertEquals("name", "This is a " + itemClass + " Name",
+						item2.getName());
+				assertEquals("description", "This is a " + itemClass
+						+ " Description", item2.getDescription());
+			}
 
-        // Persist it
-        em.getTransaction().begin();
-        em.persist(bag);
-        em.persist(cookie1);
-        em.persist(cookie2);
-        em.getTransaction().commit();
-        assertNotEquals(0L, bag.getJpaId());
-        assertNotEquals(0L, cookie1.getJpaId());
-        assertNotEquals(0L, cookie2.getJpaId());
+		}
+		em.close();
+	}
 
-        em.close();
-    }
+	@Test
+	public void testContainer() {
+		Bag bag = new Bag();
+		Cookie cookie1 = new Cookie();
+		Cookie cookie2 = new Cookie();
+		bag.add(cookie1);
+		bag.add(cookie2);
+		assertEquals(0L, cookie1.getJpaId());
+		assertEquals(0L, cookie2.getJpaId());
+		assertEquals(0L, bag.getJpaId());
 
-    public static Location testGetWorld() {
-        // Ad-hoc test world
-        Location world = new Location("World");
-        world.setWeightMax(100000);
-        world.setVolumeMax(100000);
+		final String PERSISTENCE_UNIT_NAME = "items";
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		EntityManager em = factory.createEntityManager();
 
-        // Scabbard
-        Scabbard scabbard = new Scabbard();
-        scabbard.add(new Sword());
+		// Persist it
+		em.getTransaction().begin();
+		em.persist(bag);
+		em.persist(cookie1);
+		em.persist(cookie2);
+		em.getTransaction().commit();
+		assertNotEquals(0L, bag.getJpaId());
+		assertNotEquals(0L, cookie1.getJpaId());
+		assertNotEquals(0L, cookie2.getJpaId());
 
-        Scabbard scabbard2 = new Scabbard("Scabbard2");
-        scabbard2.add(new Sword());
+		em.close();
+	}
 
-        Bag boh = new BagOfHolding(1);
-        boh.add(new Bag("Bag1"));
-        boh.add(new Box("Box1"));
-        boh.add(new Candle());
-        boh.add(new Cloak());
-        boh.add(new Cookie("Cookie1"));
-        boh.add(new Crossbow());
-        boh.add(new CrossbowBolt());
-        boh.add(new MagicRing());
-        boh.add(new Ring());
-        boh.add(scabbard);
-        boh.add(new Torch());
+	public static Location testGetWorld() {
+		// Ad-hoc test world
+		Location world = new Location("World");
+		world.setWeightMax(100000);
+		world.setVolumeMax(100000);
 
-        // a backpack of stuff
-        Bag backpack = new Backpack("Backpack1");
-        backpack.setWeightMax(100000);
-        backpack.setVolumeMax(100000);
-        backpack.add(boh);
+		// Scabbard
+		Scabbard scabbard = new Scabbard();
+		scabbard.add(new Sword());
 
-        Quiver quiver = new Quiver();
-        quiver.add(new Arrow());
-        quiver.add(new Arrow());
-        quiver.add(new Arrow());
+		Scabbard scabbard2 = new Scabbard("Scabbard2");
+		scabbard2.add(new Sword());
 
-        Bag bag2 = new Bag("Bag2");
-        bag2.add(new Cookie("Cookie6"));
-        bag2.add(new Cookie("Cookie7"));
-        bag2.add(new Cookie("Cookie8"));
-        backpack.add(bag2);
+		Bag boh = new BagOfHolding(1);
+		boh.add(new Bag("Bag1"));
+		boh.add(new Box("Box1"));
+		boh.add(new Candle());
+		boh.add(new Cloak());
+		boh.add(new Cookie("Cookie1"));
+		boh.add(new Crossbow());
+		boh.add(new CrossbowBolt());
+		boh.add(new MagicRing());
+		boh.add(new Ring());
+		boh.add(scabbard);
+		boh.add(new Torch());
 
-        // a human with a bag of cookies
-        Human human = new Human("Human1");
-        world.add(human);
-        human.setWeightMax(100000);
-        human.setVolumeMax(100000);
+		// a backpack of stuff
+		Bag backpack = new Backpack("Backpack1");
+		backpack.setWeightMax(100000);
+		backpack.setVolumeMax(100000);
+		backpack.add(boh);
 
-        human.add(new Shield());
-        human.add(scabbard2);
-        human.add(quiver);
-        human.add(backpack);
+		Quiver quiver = new Quiver();
+		quiver.add(new Arrow());
+		quiver.add(new Arrow());
+		quiver.add(new Arrow());
 
-        world.add(new Sword());
-        world.add(new Horse());
+		Bag bag2 = new Bag("Bag2");
+		bag2.add(new Cookie("Cookie6"));
+		bag2.add(new Cookie("Cookie7"));
+		bag2.add(new Cookie("Cookie8"));
+		backpack.add(bag2);
 
-        // bag3
-        Bag bag3 = new Bag("Bag3");
-        bag3.add(new Cookie("Cookie9"));
-        bag3.add(new Cookie("Cookie10"));
-        bag3.add(new Cookie("Cookie11"));
-        world.add(bag3);
+		// a human with a bag of cookies
+		Human human = new Human("Human1");
+		world.add(human);
+		human.setWeightMax(100000);
+		human.setVolumeMax(100000);
 
-        return world;
-    }
+		human.add(new Shield());
+		human.add(scabbard2);
+		human.add(quiver);
+		human.add(backpack);
 
-    @Test
-    public void testLocation() {
+		world.add(new Sword());
+		world.add(new Horse());
 
-        Location loc = testGetWorld();
+		// bag3
+		Bag bag3 = new Bag("Bag3");
+		bag3.add(new Cookie("Cookie9"));
+		bag3.add(new Cookie("Cookie10"));
+		bag3.add(new Cookie("Cookie11"));
+		world.add(bag3);
 
-        final String PERSISTENCE_UNIT_NAME = "items";
-        EntityManagerFactory factory = Persistence
-                .createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-        EntityManager em = factory.createEntityManager();
+		return world;
+	}
 
-        // Persist it
-        em.getTransaction().begin();
-        em.persist(loc);
-        em.getTransaction().commit();
-        assertNotEquals(0L, loc.getJpaId());
-        em.close();
-    }
+	@Test
+	public void testLocation() {
+
+		Location loc = testGetWorld();
+
+		final String PERSISTENCE_UNIT_NAME = "items";
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		EntityManager em = factory.createEntityManager();
+
+		// Persist it
+		em.getTransaction().begin();
+		em.persist(loc);
+		em.getTransaction().commit();
+		assertNotEquals(0L, loc.getJpaId());
+		em.close();
+	}
 
 }
