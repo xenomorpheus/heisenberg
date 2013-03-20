80c2315c9541ce394dee82dd76e621372aac887c
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2013-03-20 12:11:50 +0000

    Added unit tests.
    git-svn-id: file:///home/svn/prog/java/Heisenberg/trunk@5210 30b58a95-ce55-df11-8e28-0015176d015e

---
+++ src/test/java/au/net/hal9000/heisenberg/item/WaterTest.java
@@ -0,0 +1,32 @@
+package au.net.hal9000.heisenberg.item;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import au.net.hal9000.heisenberg.item.property.ItemProperty;
+
+public class WaterTest {
+
+    @Test
+    public void testIsHumanoidFood() {
+        Water water = new Water();
+        assertTrue("is humanoid food", ItemProperty.isHumanoidFood(water));
+    }
+
+    @Test
+    public void testSplitByWeight() {
+        final float tollerance = 0.0001f;
+        final float big = 7.0f;
+        final float small = 3.0f;
+        Water waterBig = new Water();
+        waterBig.setWeightBase(big + small);
+        waterBig.setVolumeBase(2.0f * (big + small));
+        Water waterSmall = waterBig.splitByWeight(small);
+        assertEquals("big's weight", big, waterBig.getWeight(), tollerance);
+        assertEquals("big's volume", big * 2.0f, waterBig.getVolume(),
+                tollerance);
+        assertEquals("small's weight", small, waterSmall.getWeight(),
+                tollerance);
+        assertEquals("small's volume", small * 2.0f, waterSmall.getVolume(),
+                tollerance);
+    }
+}
---
+++ src/test/java/au/net/hal9000/heisenberg/item/WoodTest.java
@@ -0,0 +1,32 @@
+package au.net.hal9000.heisenberg.item;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import au.net.hal9000.heisenberg.item.property.ItemProperty;
+
+public class WoodTest {
+
+    @Test
+    public void testIsHumanoidFood() {
+        Wood wood = new Wood();
+        assertFalse("is humanoid food", ItemProperty.isHumanoidFood(wood));
+    }
+
+    @Test
+    public void testSplitByWeight() {
+        final float tollerance = 0.0001f;
+        final float big = 7.0f;
+        final float small = 3.0f;
+        Wood woodBig = new Wood();
+        woodBig.setWeightBase(big + small);
+        woodBig.setVolumeBase(2.0f * (big + small));
+        Wood woodSmall = woodBig.splitByWeight(small);
+        assertEquals("big's weight", big, woodBig.getWeight(), tollerance);
+        assertEquals("big's volume", big * 2.0f, woodBig.getVolume(),
+                tollerance);
+        assertEquals("small's weight", small, woodSmall.getWeight(),
+                tollerance);
+        assertEquals("small's volume", small * 2.0f, woodSmall.getVolume(),
+                tollerance);
+    }
+}
