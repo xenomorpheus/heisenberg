4086074a263af8129a53a464c5bf18e5536bc06f
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2013-03-31 13:39:29 +0000

    Testing that menus are created.
    git-svn-id: file:///home/svn/prog/java/Heisenberg/trunk@5279 30b58a95-ce55-df11-8e28-0015176d015e

--- src/test/java/au/net/hal9000/heisenberg/worldeditor/WorldEditorTest.java
+++ src/test/java/au/net/hal9000/heisenberg/worldeditor/WorldEditorTest.java
@@ -1,25 +1,44 @@
 package au.net.hal9000.heisenberg.worldeditor;
 
 import static org.junit.Assert.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenuBar;
+
 import org.junit.Test;
 
 import au.net.hal9000.heisenberg.worldeditor.WorldEditor;
 
 public class WorldEditorTest {
 
-	@Test
-	public void testGetDemoWorld() {
-		/*
-		 * This unit test is here so:<br>
-		 * 1) We know the test data will build before running the UI.
-		 * 2) Code coverage.
-		 */
-		try {
-		  WorldEditor.getDemoWorld();
-		}
-		catch (Exception e){
-			fail(e.toString());
-		}
-	}
+    @Test
+    public void testGetDemoWorld() {
+        /*
+         * This unit test is here so:<br> 1) We know the test data will build
+         * before running the UI. 2) Code coverage.
+         */
+        try {
+            WorldEditor.getDemoWorld();
+        } catch (Exception e) {
+            fail(e.toString());
+        }
+    }
+
+    @Test
+    public void testGetMenus() {
+        ActionListener actionListener = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+        };
+        JMenuBar jMenuBar = WorldEditor.getMenus(actionListener);
+        assertNotNull("getMenus not null", jMenuBar);
+    }
 
 }
