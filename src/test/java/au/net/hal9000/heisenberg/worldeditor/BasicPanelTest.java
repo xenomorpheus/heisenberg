655b01d29fc4753079555a55586da1326b515f4b
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2013-03-31 13:40:15 +0000

    Testing that a BasicPanel is created.
    git-svn-id: file:///home/svn/prog/java/Heisenberg/trunk@5280 30b58a95-ce55-df11-8e28-0015176d015e

---
+++ src/test/java/au/net/hal9000/heisenberg/worldeditor/BasicPanelTest.java
@@ -0,0 +1,35 @@
+package au.net.hal9000.heisenberg.worldeditor;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JPanel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import au.net.hal9000.heisenberg.item.Human;
+import au.net.hal9000.heisenberg.util.Configuration;
+
+public class BasicPanelTest {
+
+    Configuration config = null;
+
+    @Before
+    public void setUp() {
+        try {
+            config = new Configuration("test/config/config.xml");
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+
+    }
+
+    @Test
+    public void testBasicPanel() {
+        Human human = new Human();
+        JPanel basicPanel = new BasicPanel(human, config);
+        assertNotNull("BasicPanel not null", basicPanel);
+    }
+
+}
