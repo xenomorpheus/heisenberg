4d2a0147bf9a224a1482bfb9fe13904df72c923d
bruins <bruins@30b58a95-ce55-df11-8e28-0015176d015e>
2013-08-17 08:13:32 +0000

    Update compiler in config.
    git-svn-id: file:///home/svn/prog/java/Heisenberg/trunk@5648 30b58a95-ce55-df11-8e28-0015176d015e

---
+++ src/test/java/au/net/hal9000/heisenberg/item/CatTest.java
@@ -0,0 +1,14 @@
+package au.net.hal9000.heisenberg.item;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CatTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
