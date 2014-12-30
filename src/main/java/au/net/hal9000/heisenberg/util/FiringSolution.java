eb66ecaefdc980ea9224d4981c1e0f76e7a00af5
bruins <bruins@hal9000.net.au>
2014-12-30 22:58:39 +1030

    Added code to calculate firing solution on a moving target

---
+++ src/main/java/au/net/hal9000/heisenberg/util/FiringSolution.java
@@ -0,0 +1,80 @@
+package au.net.hal9000.heisenberg.util;
+
+import org.jbox2d.common.Vec2;
+
+/**
+ * A library to calculate the firing solution to hit a target moving linearly in
+ * 2D.
+ */
+
+public class FiringSolution {
+
+    /**
+     * Return the firing solution for a projectile starting at 'src' with
+     * velocity 'v', to hit a target, 'targetPos'.
+     *
+     * @param Object
+     *            src position of shooter
+     * @param targetPos
+     *            target position
+     * @param targetVelocity
+     *            target velocity
+     * @param projectileSpeed
+     *            speed of projectile
+     * @return Object Coordinate at which to fire (and where intercept occurs)
+     * 
+     *         Credit: broofa http://stackoverflow.com/a/3487761/1647851
+     */
+    public static Vec2 calculate(Vec2 src, Vec2 targetPos, Vec2 targetVelocity,
+            float projectileSpeed) {
+        float tx = targetPos.x - src.x;
+        float ty = targetPos.y - src.y;
+        float tvx = targetVelocity.x;
+        float tvy = targetVelocity.y;
+
+        // Get quadratic equation components
+        float a = tvx * tvx + tvy * tvy - projectileSpeed * projectileSpeed;
+        float b = 2 * (tvx * tx + tvy * ty);
+        float c = tx * tx + ty * ty;
+
+        // Solve quadratic
+        Vec2 ts = quad(a, b, c); // See quad(), below
+
+        // Find smallest positive solution
+        Vec2 sol = null;
+        if (ts != null) {
+            float t0 = ts.x;
+            float t1 = ts.y;
+            float t = Math.min(t0, t1);
+            if (t < 0)
+                t = Math.max(t0, t1);
+            if (t > 0) {
+                sol = new Vec2(targetPos.x + targetVelocity.x * t, targetPos.y
+                        + targetVelocity.y * t);
+            }
+        }
+        return sol;
+    }
+
+    /**
+     * Return solutions for quadratic
+     */
+    public static Vec2 quad(float a, float b, float c) {
+        Vec2 sol = null;
+        if (Math.abs(a) < 1e-6) {
+            if (Math.abs(b) < 1e-6) {
+                sol = Math.abs(c) < 1e-6 ? new Vec2() : null;
+            } else {
+                sol = new Vec2(-c / b, -c / b);
+            }
+        } else {
+            float disc = b * b - 4 * a * c;
+            if (disc >= 0) {
+                disc = (float) Math.sqrt(disc);
+                a = 2 * a;
+                sol = new Vec2((-b - disc) / a, (-b + disc) / a);
+            }
+        }
+        return sol;
+    }
+}
---
+++ src/test/java/au/net/hal9000/heisenberg/util/FiringSolutionTest.java
@@ -0,0 +1,63 @@
+package au.net.hal9000.heisenberg.util;
+
+import static org.junit.Assert.*;
+
+import org.jbox2d.common.Vec2;
+import org.junit.Test;
+
+public class FiringSolutionTest {
+
+    /** Target not moving so aim directly at target. */
+    @Test
+    public void testCalculateStationaryTarget() {
+        Vec2 src = new Vec2(0, 0);
+        Vec2 targetPos = new Vec2(0, 1);
+        Vec2 targetVel = new Vec2(0, 0);
+        float interceptSpeed = 1;
+        Vec2 catTarget = FiringSolution.calculate(src, targetPos, targetVel,
+                interceptSpeed);
+        assertNotNull(catTarget);
+        assertTrue(targetPos.equals(catTarget));
+    }
+
+    /** Target too quick, no solution. */
+    @Test
+    public void testCalculateTargetTooQuick() {
+        Vec2 src = new Vec2(0, 0);
+        Vec2 targetPos = new Vec2(0, 1);
+        Vec2 targetVel = new Vec2(0, 5);
+        float interceptSpeed = 1;
+        Vec2 catTarget = FiringSolution.calculate(src, targetPos, targetVel,
+                interceptSpeed);
+        assertNull(catTarget);
+    }
+
+    /**
+     * A simple moving target.<br>
+     * calculate intercept({x:2, y:4}, {x:5, y:7, vx: 2, vy:1}, 5)
+     * = {x: 8, y: 8.5}
+     */
+    @Test
+    public void testCalculateSimple() {
+        Vec2 src = new Vec2(2, 4);
+        Vec2 targetPos = new Vec2(5, 7);
+        Vec2 targetVel = new Vec2(2, 1);
+        float interceptSpeed = 5;
+        Vec2 catTarget = FiringSolution.calculate(src, targetPos, targetVel,
+                interceptSpeed);
+        assertNotNull(catTarget);
+        assertTrue(new Vec2(8.0f, 8.5f).equals(catTarget));
+    }
+
+    @Test
+    public void testQuad() {
+        // solutions are -3 and +4.
+        // (x + 3) (x-4) = 0
+        // xx -x - 12 = 0
+        float a = 1;
+        float b = -1;
+        float c = -12;
+        Vec2 ts = FiringSolution.quad(a, b, c);
+        assertTrue(new Vec2(-3, +4).equals(ts) || new Vec2(+4, -3).equals(ts));
+    }
+}
