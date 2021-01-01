0d58ada5abd0310b8b89b35b4787b4ed415b46b8
Mike Bruins <xenomorpheus@users.noreply.github.com>
2021-01-01 21:42:22 +1030

    jUnit tests pass for java 8, but not java 11

---
+++ build.sh
@@ -0,0 +1,5 @@
+ 
+#  /usr/libexec/java_home -V
+#export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-12.0.7.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home"
+mvn clean test package
