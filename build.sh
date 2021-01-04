a41374f7bbabf354302ed7db4cb4b5c66258acf0
Mike Bruins <xenomorpheus@users.noreply.github.com>
2021-01-05 08:02:12 +1030

    Build script made more like travis-ci

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
  
 #  /usr/libexec/java_home -V
-#export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-12.0.7.jdk/Contents/Home"
+#export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-15.jdk/Contents/Home"
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home"
-mvn clean test package
+
+# TODO don't delete the database
+rm -fr /tmp/HeisenbergItemDb/
+
+mvn clean install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+mvn test -B
