6f5fa77ebf99d05eb837003a2148212b727ef16e
Mike Bruins <xenomorpheus@users.noreply.github.com>
2021-01-11 10:35:07 +1030

    Move to java 15

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
  
 #  /usr/libexec/java_home -V
-#export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-15.jdk/Contents/Home"
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-15.jdk/Contents/Home"
+#export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home"
 
 # TODO don't delete the database
 rm -fr /tmp/HeisenbergItemDb/
