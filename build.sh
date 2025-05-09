1170b89828cf96541c0b791b6c22d47d6b8d8d3d
xenomorpheus <xenomorpheus@users.noreply.github.com>
2025-05-10 06:39:46 +0930

    Switch to OpenJDK 23.0.2

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # /usr/libexec/java_home -V
 unset JAVA_HOME
 # export JAVA_HOME="`/usr/libexec/java_home -v 16`"
-export JAVA_HOME="/usr/local/Cellar/openjdk/23.0.1/libexec/openjdk.jdk/Contents/Home/"
+export JAVA_HOME="/usr/local/Cellar/openjdk/23.0.2/libexec/openjdk.jdk/Contents/Home/"
 
 
 # mvn clean test package -f pom.xml
