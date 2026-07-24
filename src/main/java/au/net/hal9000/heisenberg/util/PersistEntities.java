70cde6e8ad1aed04640f94c07ece5d60dcee7079
xenomorpheus <xenomorpheus@users.noreply.github.com>
2026-07-24 21:03:06 +0930

    Bugfix EntityManagerFactory created on every database operation

--- src/main/java/au/net/hal9000/heisenberg/util/PersistEntities.java
+++ src/main/java/au/net/hal9000/heisenberg/util/PersistEntities.java
@@ -12,45 +12,30 @@ public class PersistEntities {
 
   /** Persistence unit name for Entity Manager. */
   private static final String PERSISTENCE_UNIT_NAME = "items";
+  private static final EntityManagerFactory FACTORY =
+      Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 
-  /**
-   * Saves the given item to the database.
-   *
-   * @param item the item to be persisted
-   */
-  public static void save(final Item item) { // TODO List<Item> items
-
-    // Persistence Entity Manager.
-    EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-    if (factory == null) {
-      throw new RuntimeException("No JPA factory");
+  public static void save(final Item item) {
+    EntityManager entityManager = FACTORY.createEntityManager();
+    try {
+      entityManager.getTransaction().begin();
+      entityManager.persist(item);
+      entityManager.getTransaction().commit();
+      log.info("Saved");
+    } finally {
+      if (entityManager.getTransaction().isActive()) {
+        entityManager.getTransaction().rollback();
+      }
+      entityManager.close();
     }
-    EntityManager entityManager = factory.createEntityManager();
-    if (entityManager == null) {
-      throw new RuntimeException("No JPA entity manager");
-    }
-    entityManager.getTransaction().begin();
-    entityManager.persist(item);
-    entityManager.getTransaction().commit();
-    entityManager.close();
-    factory.close();
-    log.info("Saved");
   }
 
-  /**
-   * Finds an entity of the specified class by its primary key.
-   *
-   * @param <T> the type of the entity
-   * @param entityClass the class of the entity to find
-   * @param primaryKey the primary key of the entity
-   * @return the found entity or null if no entity is found
-   */
   public static <T> T find(Class<T> entityClass, Object primaryKey) {
-    EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-    EntityManager em = factory.createEntityManager();
-    var entity = em.find(entityClass, primaryKey);
-    em.close();
-    factory.close();
-    return entity;
+    EntityManager em = FACTORY.createEntityManager();
+    try {
+      return em.find(entityClass, primaryKey);
+    } finally {
+      em.close();
+    }
   }
 }
